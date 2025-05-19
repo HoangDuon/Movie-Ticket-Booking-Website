296ffecf4ddb9823809dbcd905739f67978ae1b1
Kha <146313691+bdkha985@users.noreply.github.com>
2025-05-19 15:04:04 +0700

    Update login.php

--- app/controler/login.php
+++ app/controler/login.php
@@ -6,12 +6,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // exit;   
 
     $user = new user_services();
-    $email = $_POST['username'];
-    $password = $_POST['password'];
+    $email = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
 
     $user_info = $user->login($email, $password);
 
     if ($user_info) {
+
+        unset($_SESSION['login_error_message_text']); // Xóa lỗi nếu có từ lần trước
         $_SESSION['user'] = $user_info;
         if ($user_info['role'] === 'Admin') {
             header("Location: /LTW/app/view/admin.php");
@@ -20,9 +22,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         exit();
         } else {
-            echo "Email hoặc mật khẩu không đúng!";
+            $_SESSION['login_error_message_text'] = "Thông tin đăng nhập chưa chính xác, vui lòng nhập lại.";
+            header("Location: /LTW/index.php?page=login"); // Chuyển hướng về trang login
+            exit();
         }
-
-    
 }
 ?>
